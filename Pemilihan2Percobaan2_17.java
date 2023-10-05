f6904f0fa9c867aaae512a6c8321b9c5cb3a10c4
MalikAdz22 <adzanomalik@gmail.com>
2023-10-04 21:37:29 -0800

    percobaan2

---
+++ Pemilihan2Percobaan2_17.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan2_17 {
+    public static void main(String[] args) {
+        Scanner input17 = new Scanner(System.in);
+
+        float sudut1, sudut2, sudut3, totalSudut;
+        String jenis;
+
+        System.out.println("Masukkan Sudut 1 :");
+        sudut1 =input17.nextInt();
+        System.out.println("Masukkan Sudut 2 :");
+        sudut2 =input17.nextInt();
+        System.out.println("Masukkan Sudut 3 :");
+        sudut3 =input17.nextInt();
+
+        totalSudut = sudut1 + sudut2 + sudut3;
+        
+        if(totalSudut == 180) {
+            if((sudut1==90) || (sudut2==90) || (sudut3==90)){
+                jenis = "Segitiga Siku-siku";
+            }else if((sudut1==60) && (sudut2==60) && (sudut3==60)){
+                jenis = "Segitiga Sama Sisi";
+            }else if((sudut1 == sudut2) || (sudut1==sudut3) || (sudut3==sudut2)){
+                jenis = "Segitiga Sama Kaki";
+            }else{
+                jenis = "Segitiga Sembarang";
+            }
+
+            System.out.println("Segitiga tersebut adalah "+ jenis);
+        }
+    }
+    
+}
